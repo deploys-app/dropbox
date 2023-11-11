7c77213da9ae31953bdcafbf7fa9c0d8382e3cd6
Thanatat Tamtan <acoshift@gmail.com>
2023-11-11 15:40:30 +0700

    implement

---
+++ README.md
@@ -0,0 +1,16 @@
+# Deploys.app - Dropbox
+
+Temporary file storage
+
+## Development
+
+```shell
+$ bun install
+$ bun start
+```
+
+## Deployment
+
+```shell
+$ bun run deploy
+```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * @typedef Env
+ * @property {import('@cloudflare/workers-types').R2Bucket} BUCKET
  */
 
+const baseUrl = 'https://dropbox.deploys.app/'
+
 export default {
+	/**
+	 * @param {import('@cloudflare/workers-types').Request} request
+	 * @param {Env} env
+	 * @param {import('@cloudflare/workers-types').ExecutionContext} ctx
+	 * @returns {Promise<import('@cloudflare/workers-types').Response>}
+	 **/
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+		if (request.method !== 'POST') {
+			return new Response('Deploys.app Dropbox Service')
+		}
+
+		const url = new URL(request.url)
+		if (url.pathname !== '/') {
+			return new Response('error: not found', { status: 404 })
+		}
+		if (!request.body) {
+			return new Response('error: body empty', { status: 400 })
+		}
+
+		// TODO: check auth
+
+		const fn = generateFilename()
+		try {
+			await env.BUCKET.put(fn, request.body, {
+				httpMetadata: {
+					cacheControl: 'public, max-age=86400'
+				}
+			})
+		} catch (e) {
+			return new Response('error: failed to upload', { status: 500 })
+		}
+		return new Response(JSON.stringify({
+			downloadUrl: `${baseUrl}${fn}`
+		}), {
+			headers: {
+				'content-type': 'application/json'
+			}
+		})
+	}
+}
+
+function generateFilename () {
+	const fn = new Uint8Array(64)
+	crypto.getRandomValues(fn)
+	return toRawURLEncoding(btoa(String.fromCodePoint(...fn)))
+}
+
+function toRawURLEncoding (s) {
+	return s.replace(/=*$/, '').replace(/\+/g, '-').replace(/\//g, '_')
+}
