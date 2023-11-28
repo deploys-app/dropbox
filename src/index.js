7c3b2af3be8afdaae1d523127c1c431f419b4a03
Thanatat Tamtan <acoshift@gmail.com>
2023-11-28 20:35:44 +0700

    fix lint

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ export default {
 			ok: true,
 			result: {
 				downloadUrl: `${baseUrl}${fn}`,
-				expiresAt: formatDate(expiresAt),
+				expiresAt: formatDate(expiresAt)
 			}
 		}), {
 			headers: {
